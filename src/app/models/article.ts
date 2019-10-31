78fa16a984b8f08c13f2aaa86231a8c5e75a9525
Andriy <asoha474@gmail.com>
2019-10-31 20:47:20 +0200

    added article model

---
+++ src/app/models/article.ts
@@ -0,0 +1,10 @@
+export class Article {
+
+  constructor(
+    public id: number = null,
+    public name: string = '',
+    public label: string = '',
+    public price: number = null
+  ) {
+  }
+}
