7e66913bd3bdf4cbd331881ff5903fefa40fb943
Andriy <asoha474@gmail.com>
2019-10-31 22:11:00 +0200

    add adding articles to shopping-cart variable

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Article} from '../../models/Article';
 import {ArticleService} from '../../services/article.service';
+import {CartData} from '../../models/CartData';
 
 @Component({
   selector: 'app-articles',
@@ -10,6 +11,7 @@ import {ArticleService} from '../../services/article.service';
 export class ArticlesComponent implements OnInit {
 
   articles: Article[] = [];
+  shoppingCart: CartData[] = [];
 
   constructor(private articleService: ArticleService) {
   }
@@ -24,6 +26,14 @@ export class ArticlesComponent implements OnInit {
 
 
   addToCart(article: Article) {
-
+    const item = this.shoppingCart.find(cartData => cartData.articleId === article.id);
+    if (item) {
+      item.amount += 1;
+    } else {
+      const newCartData = new CartData(article.id, 1);
+      this.shoppingCart.push(newCartData);
+    }
+    console.log(JSON.stringify(this.shoppingCart));
   }
+
 }
