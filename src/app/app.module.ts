acd38cd17e74c1a10d6ce663ab84ce66a65d159a
Andriy <asoha474@gmail.com>
2019-10-31 21:04:45 +0200

    added cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticlesComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule
